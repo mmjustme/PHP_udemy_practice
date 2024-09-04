19fddc4c9b4bd90bcd8a78f2dc5cb2943f56afe8
D <166647954+mmjustme@users.noreply.github.com>
2024-09-04 09:10:13 +0000

    Initial commit

---
+++ inc/header.inc.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./styles/simple.css" />
+    <link rel="stylesheet" href="./styles/custom.css" />
+    <title>Culinary Cove &bull; Our mission</title>
+  </head>
+  <body>
+    <header
+      class="header-with-background"
+      style="background-image: url('https://images.unsplash.com/photo-1447078806655-40579c2520d6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); "
+    >
+      <h1>Culinary Cove</h1>
+      <p>Your sanctuary for exceptional flavors</p>
+      <nav>
+        <a href="/index.php">Our mission</a>
+        <a href="/ingridients.php">Ingridients</a>
+      </nav>
+    </header>
+
+    <main>
